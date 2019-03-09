2ecbd54956c846e8d286efb892d79d04859b2e84
kapilgidwani <kapil.gidwani37@gmail.com>
2019-03-09 22:41:50 +0530

    first commit

---
+++ src/main/java/com/example/demo/docker/MyfirstdockerappApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyfirstdockerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstdockerappApplication.class, args);
+	}
+
+	@GetMapping("/msg")
+	public String getMsg() {
+		return "welcome kapil :)";
+	}
+
+}
